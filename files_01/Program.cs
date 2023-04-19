2dc23ece5dace6cffc4cd9c75529766fd1c5470d
aud404_st <aud404_st@CR404-PC12>
2023-04-19 18:24:34 +0300

    Добавьте файлы проекта.

---
+++ files_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        string fileDirection = new string("text.txt");
+
+        Console.Write("введіть дані для дозапису у файл:\n=> ");
+        string requestInputText = new string(Console.ReadLine());
+
+        using (FileStream fs = new FileStream(fileDirection, FileMode.Append))
+        {
+            using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+            {
+                sw.WriteLine(requestInputText);
+                Console.WriteLine("\nдані записано!");
+            }
+        }
+
+    }
+}
